1bb5b4071127e74acf636278ca51191fb8233590
Clément Niot <clement.niot@gmail.com>
2025-08-04 14:49:11 +0200

    [FEAT] calendrier: affichage de la vue mois avec naviguation entre les mois

--- src/app/pages/activites/calendrier/mois/mois.component.ts
+++ src/app/pages/activites/calendrier/mois/mois.component.ts
@@ -1,14 +1,108 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {User} from '../../../../models/user';
 import moment from 'moment/moment';
+import {Button} from 'primeng/button';
+import {NgClass, NgForOf} from '@angular/common';
+import {StringUtils} from '../../../../utils/string-utils';
+import {Calendrier, CalendrierJour} from '../../../../models/calendrier/calendrier-semaine';
+import {TacheService} from '../../../../services/tache.service';
+import {StatutJour} from '../../../../models/statut-jour';
 
 @Component({
   selector: 'app-mois',
-  imports: [],
+  imports: [
+    NgForOf,
+    NgClass,
+    Button
+  ],
   templateUrl: './mois.component.html',
   styleUrl: './mois.component.css'
 })
-export class MoisComponent {
+export class MoisComponent implements OnInit {
   @Input() user: User | undefined = undefined;
   @Output() onClick: EventEmitter<moment.Moment> = new EventEmitter();
+  currentDate = moment(); // mois affiché
+  days: moment.Moment[] = [];
+  calendrier: Calendrier | undefined;
+  updateDate: EventEmitter<moment.Moment> = new EventEmitter();
+
+  constructor(private stringUtils: StringUtils, private tacheService: TacheService) {
+    moment.locale('fr');
+    this.updateDate.subscribe(date => {
+      this.currentDate = date;
+      this.generateCalendar();
+      this.loadData();
+    });
+  }
+
+  ngOnInit() {
+    this.updateDate.emit(moment());
+  }
+
+  loadData() {
+    if (this.user) {
+      this.tacheService.getCalendrierMois(this.year, this.month, this.user.id).subscribe((calendrier: Calendrier) => {
+        this.calendrier = calendrier;
+      });
+    }
+  }
+
+  generateCalendar() {
+    const startOfMonth = this.currentDate.clone().startOf('month');
+    const endOfMonth = this.currentDate.clone().endOf('month');
+
+    const startDay = startOfMonth.clone().startOf('week');
+    const endDay = endOfMonth.clone().endOf('week');
+
+    const date = startDay.clone();
+    this.days = [];
+
+    while (!date.isAfter(endDay, 'day')) {
+      this.days.push(date.clone());
+      date.add(1, 'day');
+    }
+  }
+
+  get monthName() {
+    return this.stringUtils.capitalizeFirstLetter(moment.months()[this.currentDate.month()])
+  }
+
+  get month() {
+    return this.currentDate.month() + 1;
+  }
+
+  get year() {
+    return this.currentDate.year();
+  }
+
+  goToPreviousMonth() {
+    this.updateDate.emit(this.currentDate.clone().subtract(1, 'month'));
+  }
+
+  goToNextMonth() {
+    this.updateDate.emit(this.currentDate.clone().add(1, 'month'));
+  }
+
+  goToToday() {
+    this.updateDate.emit(moment());
+  }
+
+  isToday(day: moment.Moment): boolean {
+    return day.isSame(moment(), 'day');
+  }
+
+  isCurrentMonth(day: moment.Moment): boolean {
+    return day.month() === this.currentDate.month();
+  }
+
+  getCalendrierJour(day: moment.Moment): CalendrierJour | undefined {
+    if (this.calendrier) {
+      return this.calendrier.jours.find(jour => {
+        return moment(jour.jour,"DD/MM/YYYY").isSame(day, 'day');
+      });
+    }
+    return undefined;
+  }
+
+  protected readonly StatutJour = StatutJour;
 }
